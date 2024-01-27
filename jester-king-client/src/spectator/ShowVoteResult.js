7dcd76d4051436abe91dbf2615d347432218e56a
PhoenLeo <phoenleo@gmail.com>
2024-01-27 16:27:53 +0700

    🧹 cleanup unused function

--- jester-king-client/src/spectator/ShowVoteResult.js
+++ jester-king-client/src/spectator/ShowVoteResult.js
@@ -1,5 +1,5 @@
 import { useParams, useNavigate } from 'react-router-dom'
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 function ShowVoteResult() {
   const [showResult, setShowResult] = useState(false)
