834a287360e625ea89c59fc69b72e62e7412e1e0
PhoenLeo <phoenleo@gmail.com>
2024-01-28 14:21:05 +0700

    ✨ Add APIs to store

--- jester-king-client/src/store.js
+++ jester-king-client/src/store.js
@@ -14,7 +14,7 @@ const useStore = create(
     setLoading: (value) => set(() => ({ loading: value})),
 
     session: null,
-    setSession: (value) => set(() => ({ session: null })),
+    setSession: (value) => set(() => ({ session: value })),
 
     // APIs
     createSession: async() => {
@@ -32,6 +32,57 @@ const useStore = create(
       set({ loading: true })
       try {
         const res = await apiClient.get(`/session/${sessionCode}`)
+        set({ loading: false, session: res.data })
+      } catch(err) {
+        set({ error: err, loading: false })
+      }
+    },
+
+    registerPlayer: async(sessionCode, playerId, playerName) => {
+      set({ loading: true })
+      try {
+        const res = await apiClient.patch(
+          `/session/${sessionCode}/player/${playerId}/register`, 
+          { name: playerName }
+        )
+        set({ loading: false, session: res.data })
+      } catch(err) {
+        set({ error: err, loading: false })
+      }
+    },
+
+    submitPunchline: async(sessionCode, playerId, punchline) => {
+      set({ loading: true })
+      try {
+        const res = await apiClient.patch(
+          `/session/${sessionCode}/player/${playerId}/submit-punchline`, 
+          { punchline }
+        )
+        set({ loading: false, session: res.data })
+      } catch(err) {
+        set({ error: err, loading: false })
+      }
+    },
+
+    submitVote: async(sessionCode, votedPlayerId) => {
+      set({ loading: true })
+      try {
+        const res = await apiClient.patch(
+          `/session/${sessionCode}/voter/submit-vote`, 
+          { votedPlayerId }
+        )
+        set({ loading: false, session: res.data })
+      } catch(err) {
+        set({ error: err, loading: false })
+      }
+    },
+
+    closeSession: async(sessionCode) => {
+      set({ loading: true })
+      try {
+        const res = await apiClient.patch(
+          `/session/${sessionCode}/close`
+        )
         set({ loading: false, session: res.data })
       } catch(err) {
         set({ error: err, loading: false })
