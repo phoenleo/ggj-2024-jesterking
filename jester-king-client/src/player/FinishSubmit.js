124e49a27516f3f28a44ef4608860c5ce4c3b65d
PhoenLeo <phoenleo@gmail.com>
2024-01-28 15:10:17 +0700

    ✨ Update finish submit page

--- jester-king-client/src/player/FinishSubmit.js
+++ jester-king-client/src/player/FinishSubmit.js
@@ -1,20 +1,40 @@
 import { useParams, useNavigate } from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
+import useStore, { getPlayer } from '../store';
+import ErrorModal from '../utils/ErrorModal';
+import LoadingModal from '../utils/LoadingModal';
+import { useEffect, useState } from 'react';
 
 function FinishSubmit() {
-  let { sessionId } = useParams()
+  let { sessionId, playerId } = useParams()
   const navigate = useNavigate();
   const gotoNewSession = () => navigate('/')
 
-  return (
+   // Vars
+  const loading = useStore((state) => state.loading)
+  const error = useStore((state) => state.error)
+  const clearError = useStore((state) => state.clearError)
+  const session = useStore((state) => state.session)
+
+  // APIs
+  const getSession = useStore((state) => state.getSession)
+
+
+  const punchline = () => {
+    const player = getPlayer(session, playerId)
+    return player.selectedPunchline 
+  }
+
+  useEffect(() => {
+    getSession(sessionId)
+  }, [])
+
+  return session && (
     <div>
       <p>Session: {sessionId}</p>
       <p>Finish Submit</p>
-      <p>JOKE SETUP: Cita cita ___</p>
-      <p>PUNCHLINE OPTIONS:</p>
-      <ul>
-        <li>Sampah Masyarakat</li>
-      </ul>
+      <p>{ session.jokeSetup }</p>
+      <p>{ punchline() }</p>
 
       <div className='mt-2 mb-2'>
         <Button variant="danger" size="lg" onClick={gotoNewSession}>
